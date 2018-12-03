39b58f970c1f9ef9ae004882999143223d927c83
alexgui37 <alex.gui37@gmail.com>
2018-12-02 23:21:07 -0200

    Implementacao de usuarioBD e Servlet

--- src/SistemaRH/BDconfig/UsuarioBD.java
+++ src/SistemaRH/BDconfig/UsuarioBD.java
@@ -16,13 +16,13 @@ public class UsuarioBD {
 			ConexaoBD a = new ConexaoBD();
 			a.iniciaBd();
 			Connection c = a.getConexao();
-			PreparedStatement ps = (PreparedStatement) c.prepareStatement("select id_matricula, cd_cpf, ativo from funcionario where ? = id_matricula");
+			PreparedStatement ps = (PreparedStatement) c.prepareStatement("select id_matricula, cd_cpf, st_ativo, id_vaga from funcionario where ? = id_matricula");
 			ps.setString(1, mat);
 			ResultSet res1 = (ResultSet) ps.executeQuery();
 			PreparedStatement ps2 = (PreparedStatement) c.prepareStatement("select nm_nome from pessoa where ? = cpf");
 			ps2.setString(1, res1.getString("cpf"));
 			ResultSet res2 = (ResultSet) ps.executeQuery();
-			Funcionario func = new Funcionario(res1.getString("id_matricula"), res1.getString("cd_cpf"), res2.getString("nm_nome"), res1.getBoolean("ativo"));
+			Funcionario func = new Funcionario(res1.getString("id_matricula"), res1.getString("cd_cpf"), res2.getString("nm_nome"), res1.getBoolean("st_ativo"), res1.getInt("id_vaga"));
 			ps.close();
 			c.close();
 			a.fechaBd();
@@ -34,13 +34,24 @@ public class UsuarioBD {
 		}
 
 	}
-	public static synchronized boolean exoneraFunc(String mat){
+	public static synchronized boolean exoneraFunc(Funcionario func){
 		try {
 			ConexaoBD a = new ConexaoBD();
 			a.iniciaBd();
 			Connection c = a.getConexao();
-			PreparedStatement ps = (PreparedStatement) c.prepareStatement("UPDATE funcionario SET status = false WHERE id_matricula = ?");
-			ps.setString(1, mat);
+			PreparedStatement ps= (PreparedStatement) c.prepareStatement("BEGIN");
+			ps.executeQuery();
+			ps = (PreparedStatement) c.prepareStatement("UPDATE funcionario SET status = 0 WHERE id_matricula = ?");
+			ps.setString(1, func.getMatricula());
+			ps.executeQuery();
+			ps = (PreparedStatement) c.prepareStatement("UPDATE `concurso_vaga` SET `st_ocupada`= 0 WHERE id_vaga = ?");
+			ps.setInt(1, func.getIdVaga());
+			ps.executeQuery();
+			ps = (PreparedStatement) c.prepareStatement("INSERT INTO `concurso_vaga_historico`(`id_nu_vaga`,`dt_atualizacao`, `id_situacao`) VALUES (?, ?, 1)");
+			ps.setInt(1, func.getIdVaga());
+			ps.setString(2, " ");
+			ps.executeQuery();
+			ps= (PreparedStatement) c.prepareStatement("COMMIT");
 			ps.executeQuery();
 			ps.close();
 			c.close();
--- src/SistemaRH/model/Funcionario.java
+++ src/SistemaRH/model/Funcionario.java
@@ -7,15 +7,16 @@ public class Funcionario extends Pessoa{
 	private Date data_admissao;
 	private Date data_desligamento;
 	private String unidade;
-	private Vaga vaga_func;
+	private int idVaga;
 	private Date data_portaria;
 	private boolean status;
 	
-	public Funcionario(String mat, String cpf, String nome, boolean status) {
+	public Funcionario(String mat, String cpf, String nome, boolean status, int vaga) {
 		super.setNome(nome);
 		super.setCPF(cpf);
 		this.matricula = mat;
 		this.status = status;
+		this.idVaga = vaga;
 	}
 
 	public boolean isStatus() {
@@ -24,6 +25,34 @@ public class Funcionario extends Pessoa{
 
 	public void setStatus(boolean status) {
 		this.status = status;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public Especialidade getEsp_func() {
+		return esp_func;
+	}
+
+	public Date getData_admissao() {
+		return data_admissao;
+	}
+
+	public Date getData_desligamento() {
+		return data_desligamento;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public int getIdVaga() {
+		return idVaga;
+	}
+
+	public Date getData_portaria() {
+		return data_portaria;
 	}
 	
 	
