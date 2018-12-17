2e7a37d14e41515e921e587fc6d1ad892168d493
renanklein <42068283+renanklein@users.noreply.github.com>
2018-12-17 15:36:39 -0200

    Create InativaServlet.java

---
+++ src/SistemaRH/servlet/InativaServlet.java
@@ -0,0 +1,47 @@
+package SistemaRH.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import SistemaRH.BDconfig.DAO_RH1;
+import SistemaRH.model.Funcionario;
+
+/**
+ * Servlet implementation class InativaServlet
+ */
+public class InativaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InativaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String mat = request.getParameter("matricula");
+		boolean isExonerado = DAO_RH1.exoneraFunc(mat);
+		HttpSession session =  request.getSession();
+		//session.setAttribute("func", func);
+		response.sendRedirect("res.jsp");
+
+	}
+
+}
